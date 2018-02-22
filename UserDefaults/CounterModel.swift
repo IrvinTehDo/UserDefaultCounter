c3e8ddb2784d5d49121733855b41a03e8bfeac08
Student <student@raj16.istlabs.rit.edu>
2018-02-22 18:51:34 -0500

    First Commit

--- UserDefaults/CounterModel.swift
+++ UserDefaults/CounterModel.swift
@@ -2,58 +2,8 @@
 //  CounterModel.swift
 //  UserDefaults
 //
-//  Created by Paul Solt on 2/22/18.
+//  Created by Student on 2/22/18.
 //  Copyright © 2018 Paul Solt. All rights reserved.
 //
 
 import Foundation
-
-let kCounterKey = "counterKey"
-let kDateKey = "dateKey"
-
-protocol CounterModel {
-    var counter: Int { get set }
-    var dateModified: Date? { get set }
-    func save()
-    func load()
-}
-
-class CounterModelUserDefaults: CounterModel {
-    let defaults: UserDefaults
-    
-    var counter: Int
-    var dateModified: Date?
-    
-    init(userDefaults: UserDefaults = UserDefaults.standard) {
-        self.defaults = userDefaults
-        
-        counter = -1
-        dateModified = nil
-        
-        load()
-    }
-    
-    func save() {
-        print(#function)
-        defaults.set(counter, forKey: kCounterKey)
-        defaults.set(dateModified, forKey: kDateKey)
-//        defaults.synchronize()
-    }
-    
-    func load() {
-        if let counter = defaults.value(forKey: kCounterKey) as? Int {
-            print("Loaded counter: \(counter)")
-            self.counter = counter
-        } else {
-            print("nil counter for key: \(kCounterKey)")
-        }
-        
-        if let date = defaults.value(forKey: kDateKey) as? Date {
-            print("Loaded date: \(date)")
-            self.dateModified = date
-        } else {
-            print("nil date for key: \(kDateKey)")
-        }
-        
-    }
-}
